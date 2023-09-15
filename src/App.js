b4b8f998a23d0dbfc7c20cb9cbda71f29bd47205
pjca004 <jcanale@epri.com>
2023-09-15 11:11:36 -0400

    Up to Chapter 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header';
+import { items } from './items';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home items={items} />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import Thumbnail from "./Thumbnail";
+import { itemImages } from "../items";
+import './Home.css';
+
+function Home ({items}){
+  return(
+    <div className="home-component">
+      { items.map((item) => (
+        <Thumbnail 
+         key={item.itemId}
+         image={itemImages[item.imageId]} 
+         title={item.title}
+        />        
+      ))}
+    </div>
+  );
+}
+export default Home;
