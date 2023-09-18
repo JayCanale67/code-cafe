4d8cc1bf8fd1bad28c82bb286912a087129b652a
pjca004 <jcanale@epri.com>
2023-09-18 15:06:04 -0400

    Up to Chapter 11

--- src/components/DetailItem.js
+++ src/components/DetailItem.js
@@ -7,13 +7,21 @@ function DetailItem({ items }) {
   const detailItem = items.find((item) => item.itemId === id);
   return (
     <div className="detail-item-component">
-      <img
-        className="details-image"
-        src={itemImages[detailItem.imageId]}
-        alt={itemImages.coffee}
-      />
-      <h2>{detailItem.title}</h2>
-      <div>$ {detailItem.price.toFixed(2)}</div>
+      {detailItem ? (
+        <>
+          {detailItem.salePrice ? <div>Sale Price!</div> : ""}
+          <img
+            className="details-image"
+            src={itemImages[detailItem.imageId]}
+            alt={itemImages.coffee}
+          />
+          <h2>{detailItem.title}</h2>
+          {detailItem.description && <h4>{detailItem.description}</h4>}
+          <div>${(detailItem.salePrice ?? detailItem.price).toFixed(2)}</div>
+        </>
+      ) : (
+        <h2>Unknown Item</h2>
+      )}
     </div>
   );
 }
